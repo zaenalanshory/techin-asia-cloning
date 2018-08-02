036da11cd1b68b92984c9dea8904f262563b2769
mirzachilman <mirzachilman@gmail.com>
2018-08-02 08:13:05 +0700

    basic

--- src/Components/Kartu/Kartu.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import KartuBanyak from './KartuBanyak/KartuBanyak';
-
-const kartu = props => {
-  console.log(props);
-  return <KartuBanyak {...props} />;
-};
-
-export default kartu;
--- src/Components/Navigation/HeaderNavigation/HeaderNavigation.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const headerNavigation = () => {
-  return <h1>This is Header</h1>;
-};
-
-export default headerNavigation;
